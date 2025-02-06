5a358dc3c1c957b5f6b07eb3371141e865366c5b
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-05 19:17:58 -0600

    Carpeta src y archivo funciones.php añadidos

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -9,17 +9,11 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
+        include 'src/funciones.php';
         if(isset($_GET['numero']))
         {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+            es_multitplo5y7($_GET['numero']);
+
         }
     ?>
 
