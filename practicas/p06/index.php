85c90c798d07dad6d0a3d978b3e29fe2eccefa7c
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-09 01:17:48 -0600

    Archivos funciones.php e index.php modificados (Ejercicio 2)

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -3,18 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 4</title>
+    <title>Práctica 6</title>
 </head>
 <body>
+    <h1>PRÁCTICA 6</h1>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        include 'src/funciones.php';
+        include_once 'src/funciones.php'; //Incluimos el archivo funciones.php
         if(isset($_GET['numero']))
         {
-            es_multitplo5y7($_GET['numero']);
+            es_multiplo5y7($_GET['numero']);
 
         }
+    ?>
+    <h2>Ejercicio 2</h2>
+    <p>
+        Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+        secuencia compuesta por:
+    </p>
+    <?php
+        include_once 'src/funciones.php';
+        matrizAleatorios();
+
+
     ?>
 
     <h2>Ejemplo de POST</h2>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,5 +1,5 @@
 <?php
-    function es_multitplo5y7($num){
+    function es_multiplo5y7($num){
         if ($num%5==0 && $num%7==0)
             {
                 echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
@@ -8,6 +8,46 @@
             {
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
+    }
+
+    function matrizAleatorios(){
+        $matriz=[];
+        $totalnum=0;
+
+        for($i=0; ; $i++){
+            $fila=[];
+            
+            for($j=0; ; $j++){
+                $n=rand(100, 999);
+                $fila[] = $n;
+                $totalnum++;
+            }
+
+        if(impar($fila[0]) && par($fila[1]) && impar($fila[2])){
+            $matriz[] = $fila;
+            break;
+        }
+    }
+
+    echo "Matriz de números aleatorios (impar, par, impar): ";
+    foreach($matriz as $fila){
+        for($i=0; $i<count($fila); $i++){
+            echo $fila[1];
+            if($i<count($fila)-1){
+                echo ',';
+            }
+        }
+        echo '</br>';
+    }
+    echo '</br>Cantidad de números generados: $totalnum </br>';
+}
+
+    function impar($n){ //Función para determinar si el número es impar
+        return $n % 2 != 0;
+    }
+
+    function par($n){ //Función para determinar si el número es par
+        return $n % 2 == 0;
     }
 
 ?>
