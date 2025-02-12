9e5abfbb3289590f2a2775be029f6507903f71fd
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-11 22:16:36 -0600

    Archivo index.php modificado (Ejercicio 2)

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -4,30 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Práctica 6</title>
+    <style>
+        body{
+            background-color: lightsteelblue;
+            padding: 20px;
+        }
+        h1{
+            font-size: 40px;
+            font-weight: bold;
+            color: white;
+            padding: 10px;
+            border: 5px solid;
+            text-align: center;
+            text-shadow: 3px 3px 2px grey;
+        }
+        h2{
+            background-color: grey;
+            color: whitesmoke;
+        }
+    </style>
 </head>
 <body>
     <h1>PRÁCTICA 6</h1>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php
-        include_once 'src/funciones.php'; //Incluimos el archivo funciones.php
+        require_once 'src/funciones.php'; //Incluimos el archivo funciones.php
         if(isset($_GET['numero']))
         {
             es_multiplo5y7($_GET['numero']);
-
         }
     ?>
     <h2>Ejercicio 2</h2>
     <p>
         Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
-        secuencia compuesta por:
+        secuencia compuesta por:<br><br>
+        <strong><center>Impar, Par, Impar</center></strong>
     </p>
     <?php
-        include_once 'src/funciones.php';
-        matrizAleatorios();
+        $res = matrizSecuencia();
 
-
+        echo $res['numerosGenerados'].' números obtenidos en '. $res['iteraciones'].' iteraciones.<br>';
+        echo '<br><table border="5">';
+        foreach ($res['matriz'] as $fila) {
+            echo '<tr>';
+            foreach ($fila as $numero) {
+                echo '<td>' . $numero . '</td>';
+            }
+        echo '</tr>';
+        }
+        echo '</table>';
     ?>
+
+    <h2>Ejercicio 3</h2>
+    <p>
+        Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+        pero que además sea múltiplo de un número dado.
+    </p>
+
+    <h2>Ejercicio 4</h2>
+    <p>
+        Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’
+        a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner
+        el valor en cada índice.
+    </p>
+
+    <h2>Ejercicio 5</h2>
+    <p>
+        Usar las variables $edad y $sexo en una instrucción if para identificar una persona de
+        sexo “femenino”, cuya edad oscile entre los 18 y 35 años y mostrar un mensaje de
+        bienvenida apropiado.
+    </p>
+
+
+    <h2>Ejercicio 6</h2>
+    <p>
+        Crea en código duro un arreglo asociativo que sirva para registrar el parque vehicular de
+        una ciudad.
+    </p>
+
 
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
