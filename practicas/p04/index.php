01a0b58b312e0c936562d31a724f49c239109f6c
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-18 23:51:53 -0600

    Verificación añadida en el archivo index.php

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -264,5 +264,11 @@
     
         ?>
     </div>
+    <div>
+        <p>
+            <a href="https://validator.w3.org/markup/check?uri=referer"><img
+            src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+        </p>
+    </div>
 </body>
 </html>
