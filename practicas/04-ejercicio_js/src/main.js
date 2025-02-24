a717911ba63265931f8adb23c68b0090ea09a535
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-23 23:53:31 -0600

    Archivos index.html y main.js modificados (Ejercicio 16)

--- practicas/04-ejercicio_js/src/main.js
+++ practicas/04-ejercicio_js/src/main.js
@@ -239,6 +239,18 @@ function mostrarMensajeFu() {
     mostrarMensajeFu();
 
 //Función ejemplo 16:
+function mostrarRango(x1,x2) {
+    var inicio;
+    for(inicio=x1; inicio<=x2; inicio++) {
+        document.write(inicio+' ');
+    }
+    }
+    var valor1,valor2;
+    valor1 = prompt('Ingresa el valor inferior:', '');
+    valor1 = parseInt(valor1);
+    valor2 = prompt('Ingresa el valor superior:', '');
+    valor2 = parseInt(valor2);
+    mostrarRango(valor1,valor2);
 
 //Función ejemplo 17:
 
