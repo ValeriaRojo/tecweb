4d1f74cb15748d06dc72d50e24c7ef3f32303ac7
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-23 23:20:58 -0600

    Archivos index.html y main.js modificados (Ejercicio 7)

--- practicas/04-ejercicio_js/src/main.js
+++ practicas/04-ejercicio_js/src/main.js
@@ -87,6 +87,33 @@ function esMayor(){
     }
 }
 //Función ejemplo 7:
+function promedio(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+
+    //Convertimos los 3 string en enteros
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+    if (pro>=7) {
+            document.write('aprobado');
+        }
+
+        else {
+            if (pro>=4) {
+                document.write('regular');
+            }
+            else {
+                document.write('reprobado');
+            }
+        }
+}
 
 //Función ejemplo 8:
 
