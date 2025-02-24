08d09e408847be75a157b479038dcffcaf6356f3
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-23 22:56:59 -0600

    Archivos index.html y main.js modificados (Ejercicio 1)

--- practicas/04-ejercicio_js/src/main.js
+++ practicas/04-ejercicio_js/src/main.js
@@ -1,4 +1,3 @@
-
 function getDatos()
 {
     var nombre = prompt("Nombre: ", "");
@@ -11,3 +10,42 @@ function getDatos()
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
 }
+
+//Función ejemplo 1:
+function mostrarHolaMundo(){
+    document.write("Hola Mundo :3");
+}
+
+//Función ejemplo 2:
+
+//Función ejemplo 3:
+
+//Función ejemplo 4:
+
+//Función ejemplo 5:
+
+//Función ejemplo 6:
+
+//Función ejemplo 7:
+
+//Función ejemplo 8:
+
+//Función ejemplo 9:
+
+//Función ejemplo 10:
+
+//Función ejemplo 11:
+
+//Función ejemplo 12:
+
+//Función ejemplo 13:
+
+//Función ejemplo 14:
+
+//Función ejemplo 15:
+
+//Función ejemplo 16:
+
+//Función ejemplo 17:
+
+//Función ejemplo 18:
