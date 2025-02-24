986ef8df1e682201283b44cd598e03398fe78a47
ValeriaRojo <rh202354373@alm.buap.mx>
2025-02-23 23:11:35 -0600

    Archivos index.html y main.js modificados (Ejercicio 2)

--- practicas/04-ejercicio_js/src/main.js
+++ practicas/04-ejercicio_js/src/main.js
@@ -17,15 +17,75 @@ function mostrarHolaMundo(){
 }
 
 //Función ejemplo 2:
+function mostrarDatos(){
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+}
 
 //Función ejemplo 3:
+function ingresarDatosTeclado(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años.');
+
+}
 
 //Función ejemplo 4:
+function numeros(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
 
 //Función ejemplo 5:
+function aprobado(){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>=4) {
+    document.write(nombre+' esta aprobado con un '+nota);
+    }
+}
 
 //Función ejemplo 6:
-
+function esMayor(){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+    document.write('el mayor es '+num1);
+    }
+    else {
+    document.write('el mayor es '+num2);
+    }
+}
 //Función ejemplo 7:
 
 //Función ejemplo 8:
