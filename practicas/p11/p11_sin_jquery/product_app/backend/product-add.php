8e5e2632c343d64bc5b726e04a8a8beea23ff5f6
ValeriaRojo <rh202354373@alm.buap.mx>
2025-03-12 22:48:28 -0600

    Archivos modificados (Ejercicio 1)

--- practicas/p11/p11_sin_jquery/product_app/backend/product-add.php
+++ practicas/p11/p11_sin_jquery/product_app/backend/product-add.php
@@ -16,7 +16,9 @@
         
         if ($result->num_rows == 0) {
             $conexion->set_charset("utf8");
-            $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
+            $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}',
+                                                         {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', 
+                                                         {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
             if($conexion->query($sql)){
                 $data['status'] =  "success";
                 $data['message'] =  "Producto agregado";
