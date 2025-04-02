da3d60901372a6a175a606f57fe1a2b053773f86
ValeriaRojo <rh202354373@alm.buap.mx>
2025-04-01 19:35:02 -0600

    Cambios añadidos en view, respecto a su funcionalidad.

--- Actividades/a07_MVC/p11_con_jquery/product_app/backend/myapi/view.php
+++ Actividades/a07_MVC/p11_con_jquery/product_app/backend/myapi/view.php
@@ -1,3 +1,8 @@
 <?php
-
+    class View {
+        public function mostrar(string $data): void {
+            header('Content-Type: application/json; charset=utf-8');
+            echo $data;
+        }
+    }
 ?>
